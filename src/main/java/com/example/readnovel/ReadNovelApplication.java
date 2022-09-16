dc312773192e76ffeaeac5da44dff2dadd7e6371
thanhbc <buichithanh24122000@gmail.com>
2022-09-16 10:58:00 +0700

    fix authen va 1 so khac

--- src/main/java/com/example/readnovel/ReadNovelApplication.java
+++ src/main/java/com/example/readnovel/ReadNovelApplication.java
@@ -36,7 +36,8 @@ public class ReadNovelApplication {
         return new WebMvcConfigurerAdapter() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**").allowedOrigins("http://localhost:3000");
+                registry.addMapping("/**").allowedOrigins("http://localhost:3000")
+                        .allowedOrigins("http://127.0.0.1:5500");
             }
         };
     }
