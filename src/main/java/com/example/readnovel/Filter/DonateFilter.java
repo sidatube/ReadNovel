f1abd5d6feb546f89962e41d5b5a6ba512aba452
thanhbc <buichithanh24122000@gmail.com>
2022-09-21 20:56:41 +0700

    add sort

--- src/main/java/com/example/readnovel/Filter/DonateFilter.java
+++ src/main/java/com/example/readnovel/Filter/DonateFilter.java
@@ -15,6 +15,6 @@ public class DonateFilter {
     private String code;
     private int index=1;
     private int size=20;
-    private Date startDate;
-    private Date endDate=new Date(System.currentTimeMillis());
+//    private Date startDate;
+//    private Date endDate=new Date(System.currentTimeMillis());
 }
